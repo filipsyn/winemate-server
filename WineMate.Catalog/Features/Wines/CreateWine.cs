3a039b64c71c7621b84b27ae5754429d51402969
Filip S <filip.syn@protonmail.com>
2024-01-22 19:44:38 +0100

    Implement command validation in CreateWine use-case

--- WineMate.Catalog/Features/Wines/CreateWine.cs
+++ WineMate.Catalog/Features/Wines/CreateWine.cs
@@ -1,6 +1,13 @@
 using Carter;
+
+using ErrorOr;
+
+using FluentValidation;
+
 using Mapster;
+
 using MediatR;
+
 using WineMate.Catalog.Contracts;
 using WineMate.Catalog.Database;
 using WineMate.Catalog.Database.Entities;
@@ -9,7 +16,7 @@ namespace WineMate.Catalog.Features.Wines;
 
 public static class CreateWine
 {
-    public class Command : IRequest<Guid>
+    public class Command : IRequest<ErrorOr<Guid>>
     {
         public required string Name { get; set; }
         public string? Description { get; set; }
@@ -17,17 +24,35 @@ public static class CreateWine
         public WineType Type { get; set; } = WineType.Other;
     }
 
-    internal sealed class Handler : IRequestHandler<Command, Guid>
+    public class Validator : AbstractValidator<Command>
+    {
+        public Validator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Year).InclusiveBetween(1800, DateTime.UtcNow.Year);
+        }
+    }
+
+    internal sealed class Handler : IRequestHandler<Command, ErrorOr<Guid>>
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly IValidator<Command> _validator;
 
-        public Handler(ApplicationDbContext dbContext)
+        public Handler(ApplicationDbContext dbContext, IValidator<Command> validator)
         {
             _dbContext = dbContext;
+            _validator = validator;
         }
 
-        public async Task<Guid> Handle(Command request, CancellationToken cancellationToken)
+        public async Task<ErrorOr<Guid>> Handle(Command request, CancellationToken cancellationToken)
         {
+            var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+
+            if (!validationResult.IsValid)
+            {
+                return Error.Failure(nameof(CreateWine), validationResult.ToString() ?? "Validation failed.");
+            }
+
             var wine = new Wine
             {
                 Name = request.Name,
@@ -54,14 +79,18 @@ public class CreateWineEndpoint : ICarterModule
             {
                 var command = request.Adapt<CreateWine.Command>();
 
-                var wineId = await sender.Send(command);
+                var result = await sender.Send(command);
+                if (result.IsError)
+                {
+                    return Results.BadRequest(result.Errors);
+                }
 
                 var response = new CreateWineResponse
                 {
-                    WineId = wineId
+                    WineId = result.Value
                 };
 
-                return TypedResults.Created($"/wines/{wineId}", response);
+                return Results.Created($"/wines/{response.WineId}", response);
             })
             .WithOpenApi()
             .WithName("CreateWine")
