eaf96922f36da8cc6a226cf7e690b0e49fc1b21a
Filip S <filip.syn@protonmail.com>
2024-01-26 16:53:05 +0100

    Add `Address` entity

---
+++ WineMate.Catalog/Database/Entities/Address.cs
@@ -0,0 +1,9 @@
+namespace WineMate.Catalog.Database.Entities;
+
+public class Address
+{
+    public int Number { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+}
