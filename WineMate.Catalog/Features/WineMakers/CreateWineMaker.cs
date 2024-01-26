4581cc40e9737a0c0fc946e08986a168c5eeeed6
Filip S <filip.syn@protonmail.com>
2024-01-26 20:13:23 +0100

    Add CreateWineMaker use-case

---
+++ WineMate.Catalog/Features/WineMakers/CreateWineMaker.cs
@@ -0,0 +1,101 @@
+using Carter;
+
+using ErrorOr;
+
+using FluentValidation;
+
+using Mapster;
+
+using MediatR;
+
+using WineMate.Catalog.Contracts;
+using WineMate.Catalog.Database;
+using WineMate.Catalog.Database.Entities;
+using WineMate.Catalog.Extensions;
+using WineMate.Catalog.Validators;
+
+namespace WineMate.Catalog.Features.WineMakers;
+
+public class CreateWineMaker
+{
+    public class Command : IRequest<ErrorOr<Guid>>
+    {
+        public string Name { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class Validator : AbstractValidator<Command>
+    {
+        public Validator()
+        {
+            RuleFor(command => command.Name)
+                .NotEmpty();
+
+            RuleFor(command => command.Address)
+                .SetValidator(new AddressValidator());
+        }
+    }
+
+    internal sealed class Handler : IRequestHandler<Command, ErrorOr<Guid>>
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IValidator<Command> _validator;
+
+        public Handler(ApplicationDbContext dbContext, IValidator<Command> validator)
+        {
+            _dbContext = dbContext;
+            _validator = validator;
+        }
+
+        public async Task<ErrorOr<Guid>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+            if (!validationResult.IsValid)
+            {
+                return Error.Validation(nameof(CreateWineMaker), validationResult.ToString() ?? "Validation failed.");
+            }
+
+            var wineMaker = new WineMaker
+            {
+                Name = request.Name,
+                Address = request.Address
+            };
+
+            await _dbContext.WineMakers.AddAsync(wineMaker, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return wineMaker.Id;
+        }
+    }
+}
+
+public class CreateWineMakerEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/winemakers", async (
+                CreateWineMakerRequest request,
+                IValidator<CreateWineMakerRequest> validator,
+                ISender sender
+            ) =>
+            {
+                var validationResult = await validator.ValidateAsync(request);
+                if (!validationResult.IsValid)
+                {
+                    return Results.ValidationProblem(validationResult.ToDictionary());
+                }
+
+                var command = request.Adapt<CreateWineMaker.Command>();
+                var result = await sender.Send(command);
+
+                return result.Match(
+                    wineMakerId => Results.Created($"/winemakers/{wineMakerId}", wineMakerId),
+                    error => error.ToResponse());
+            })
+            .WithOpenApi()
+            .WithName("CreateWineMaker")
+            .WithSummary("Creates wine maker")
+            .WithDescription("Creates a new wine maker.")
+            .WithTags("WineMakers");
+    }
+}
