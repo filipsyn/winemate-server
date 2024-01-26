8f24e553fc9ed6d736e74ab851ac580e3b798ec7
Filip Synek <123979902+filipsyn@users.noreply.github.com>
2024-01-26 20:53:30 +0100

    Add GetWineMaker use-case (#11)
    * Add `WineMakerDetailResponse` contract
    
    * Add `GetWineMaker` use-case

---
+++ WineMate.Catalog/Features/WineMakers/GetWineMaker.cs
@@ -0,0 +1,71 @@
+using Carter;
+
+using ErrorOr;
+
+using Mapster;
+
+using MediatR;
+
+using Microsoft.EntityFrameworkCore;
+
+using WineMate.Catalog.Contracts;
+using WineMate.Catalog.Database;
+using WineMate.Catalog.Extensions;
+
+namespace WineMate.Catalog.Features.WineMakers;
+
+public static class GetWineMaker
+{
+    public class Query : IRequest<ErrorOr<WineMakerDetailResponse>>
+    {
+        public Guid Id { get; set; }
+    }
+
+    internal sealed class Handler : IRequestHandler<Query, ErrorOr<WineMakerDetailResponse>>
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger<Handler> _logger;
+
+        public Handler(ApplicationDbContext dbContext, ILogger<Handler> logger)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
+        public async Task<ErrorOr<WineMakerDetailResponse>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var wineMaker = await _dbContext.WineMakers
+                .FirstOrDefaultAsync(wineMaker => wineMaker.Id == request.Id, cancellationToken);
+
+            if (wineMaker is null)
+            {
+                _logger.LogWarning("Wine maker with id {Id} not found", request.Id);
+                return Error.NotFound("GetWineMaker", $"Wine maker with id {request.Id} not found.");
+            }
+
+            return wineMaker.Adapt<WineMakerDetailResponse>();
+        }
+    }
+}
+
+public class GetWineMakerEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/winemakers/{id}", async (Guid id, ISender sender) =>
+            {
+                var query = new GetWineMaker.Query { Id = id };
+
+                var result = await sender.Send(query);
+
+                return result.MatchFirst(
+                    TypedResults.Ok,
+                    error => error.ToResponse());
+            })
+            .WithOpenApi()
+            .WithName("GetWineMaker")
+            .WithSummary("Get wine maker")
+            .WithDescription("Get a wine maker by id.")
+            .WithTags("WineMakers");
+    }
+}
