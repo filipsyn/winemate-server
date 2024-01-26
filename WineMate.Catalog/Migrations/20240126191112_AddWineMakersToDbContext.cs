fb8e493d05352dcfc43642d7cae829dd004da1c1
Filip Synek <123979902+filipsyn@users.noreply.github.com>
2024-01-26 20:18:42 +0100

    Add CreateWineMaker use-case (#9)
    * Add `WineMakers` to database context
    
    * Add address validator
    
    * Add `CreatewWineMakerRequest` contract
    
    * Create `CreateWineMakerRequestValidator`
    
    * Use UTC timestamp for `CreatedAt` value
    
    * Migrate addition of `WineMakers` to database context
    
    * Add CreateWineMaker use-case
    
    * Fix summary on `CreateWineMakerEndpoint`

---
+++ WineMate.Catalog/Migrations/20240126191112_AddWineMakersToDbContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WineMate.Catalog.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWineMakersToDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wines_WineMaker_WineMakerId",
+                table: "Wines");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_WineMaker",
+                table: "WineMaker");
+
+            migrationBuilder.RenameTable(
+                name: "WineMaker",
+                newName: "WineMakers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_WineMakers",
+                table: "WineMakers",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wines_WineMakers_WineMakerId",
+                table: "Wines",
+                column: "WineMakerId",
+                principalTable: "WineMakers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wines_WineMakers_WineMakerId",
+                table: "Wines");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_WineMakers",
+                table: "WineMakers");
+
+            migrationBuilder.RenameTable(
+                name: "WineMakers",
+                newName: "WineMaker");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_WineMaker",
+                table: "WineMaker",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wines_WineMaker_WineMakerId",
+                table: "Wines",
+                column: "WineMakerId",
+                principalTable: "WineMaker",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
