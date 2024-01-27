40ff979f65ef6864f11110961a87507d6c82a834
Filip S <filip.syn@protonmail.com>
2024-01-27 13:27:44 +0100

    Change types in `WineReview` entity

--- WineMate.Reviews/Database/Entities/WineReview.cs
+++ WineMate.Reviews/Database/Entities/WineReview.cs
@@ -4,6 +4,6 @@ public class WineReview : BaseEntity
 {
     public Guid WineId { get; set; }
     public string? Title { get; set; }
-    public string Body { get; set; }
-    public string Rating { get; set; }
+    public string? Body { get; set; }
+    public int Rating { get; set; } = 0;
 }
