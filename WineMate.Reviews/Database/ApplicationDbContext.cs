50f3510b6efaec1ebb0414f706fa928ed324fa75
Filip S <filip.syn@protonmail.com>
2024-01-27 13:25:31 +0100

    Add `WineReviews` to database context

--- WineMate.Reviews/Database/ApplicationDbContext.cs
+++ WineMate.Reviews/Database/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 
+using WineMate.Reviews.Database.Entities;
+
 namespace WineMate.Reviews.Database;
 
 public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<WineReview> WineReviews => Set<WineReview>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
