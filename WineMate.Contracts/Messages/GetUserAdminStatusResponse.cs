c94b558a5eb24e35f6e7d2e53d02a6220e1cd818
Filip S <filip.syn@protonmail.com>
2024-01-28 14:49:35 +0100

    Update responses of `GetUserAdminStatus` message

--- WineMate.Contracts/Messages/GetUserAdminStatusResponse.cs
+++ WineMate.Contracts/Messages/GetUserAdminStatusResponse.cs
@@ -3,5 +3,6 @@ namespace WineMate.Contracts.Messages;
 public record GetUserAdminStatusResponse
 {
     public Guid UserId { get; init; }
+    public bool Exists { get; init; }
     public bool IsAdmin { get; init; }
 }
--- WineMate.Contracts/Messages/UserNotFoundResponse.cs
+++
@@ -1,6 +0,0 @@
-namespace WineMate.Contracts.Messages;
-
-public record UserNotFoundResponse
-{
-    public Guid UserId { get; init; }
-}
