b22f17a379efae35ea4730f6bc58d50b8ac0c3fd
Filip S <filip.syn@protonmail.com>
2024-01-28 14:50:54 +0100

    Implement IsAdmin policy

---
+++ WineMate.Catalog/Configuration/Policies/Policies.cs
@@ -0,0 +1,6 @@
+namespace WineMate.Catalog.Configuration.Policies;
+
+public static class Policies
+{
+    public const string IsAdmin = "IsAdmin";
+}
---
+++ WineMate.Catalog/Configuration/Policies/Requirements/IsAdminRequirement.cs
@@ -0,0 +1,5 @@
+using Microsoft.AspNetCore.Authorization;
+
+namespace WineMate.Catalog.Configuration.Policies.Requirements;
+
+public class IsAdminRequirement : IAuthorizationRequirement { }
