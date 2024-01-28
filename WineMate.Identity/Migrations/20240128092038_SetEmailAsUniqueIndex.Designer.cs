15c59b8fe649a664f0e8db60e4dbe4036a3b60d8
Filip S <filip.syn@protonmail.com>
2024-01-28 10:21:43 +0100

    Add migration of `SetEmailAsUniqueIndex`

--- WineMate.Identity/Migrations/20240128092038_SetEmailAsUniqueIndex.Designer.cs
+++ WineMate.Identity/Migrations/20240128092038_SetEmailAsUniqueIndex.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WineMate.Identity.Database;
@@ -11,9 +12,11 @@ using WineMate.Identity.Database;
 namespace WineMate.Identity.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240128092038_SetEmailAsUniqueIndex")]
+    partial class SetEmailAsUniqueIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
