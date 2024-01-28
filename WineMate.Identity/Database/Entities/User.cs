1a0b80ed058ebe378e374916e84a96e18f800b83
Filip S <filip.syn@protonmail.com>
2024-01-28 09:37:26 +0100

    Make `LastLoginAt` prop nullable

--- WineMate.Identity/Database/Entities/User.cs
+++ WineMate.Identity/Database/Entities/User.cs
@@ -4,6 +4,6 @@ public class User : BaseEntity
 {
     public required string Email { get; set; }
     public required string Password { get; set; }
-    public DateTime LastLoginAt { get; set; }
+    public DateTime? LastLoginAt { get; set; } = null;
     public bool IsAdmin { get; set; } = false;
 }
