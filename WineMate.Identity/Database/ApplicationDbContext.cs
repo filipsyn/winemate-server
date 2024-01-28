13754981f953cd63cf5cf4243f468d142ac9a30b
Filip S <filip.syn@protonmail.com>
2024-01-28 09:31:56 +0100

    Add data context

---
+++ WineMate.Identity/Database/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace WineMate.Identity.Database;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
--- WineMate.Identity/Program.cs
+++ WineMate.Identity/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+using WineMate.Identity.Database;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Database"));
+});
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
